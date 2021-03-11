84c6d8ae9cb5c4da9c8bf2c922b6e91ef714e634
firasbouhamda <71328772+firasbouhamda@users.noreply.github.com>
2021-03-11 13:44:00 +0100

    first commit

---
+++ program.js
@@ -0,0 +1,14 @@
+// let fs = require('fs');
+// console.log(fs)
+
+let string = 'HELLO WORLD';
+string;
+console.log()
+
+// var args = process.argv;
+
+// args = args.splice(2, args.length);
+// var answer = args.reduce(function(a,b){
+//     return +a + +b;
+// });
+// console.log(anser)
